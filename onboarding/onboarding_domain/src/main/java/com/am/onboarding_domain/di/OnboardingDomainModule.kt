25fb04fb723cca79d16ffb31d849a7b9a78d8e1a
AlexJMcD <AlexMcD@protonmail.com>
2022-08-29 11:41:04 +0100

    Tracker overview view model, including logic for viewing foods for different days, adding and removing foods etc. Added image resources for the meal types.

--- onboarding/onboarding_domain/src/main/java/com/am/onboarding_domain/di/OnboardingDomainModule.kt
+++ onboarding/onboarding_domain/src/main/java/com/am/onboarding_domain/di/OnboardingDomainModule.kt
@@ -4,7 +4,6 @@ import com.am.onboarding_domain.use_case.ValidateNutrients
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
---
+++ tracker/tracker_presentation/src/main/java/com/am/tracker_presentation/tracker_overview/TrackerOverviewEvent.kt
@@ -0,0 +1,11 @@
+package com.am.tracker_presentation.tracker_overview
+
+import com.am.tracker_domain.model.TrackedFood
+
+sealed class TrackerOverviewEvent {
+    object OnNextDayClick: TrackerOverviewEvent()
+    object OnPreviousDayClick: TrackerOverviewEvent()
+    data class OnToggleMealClick(val meal: Meal): TrackerOverviewEvent()
+    data class OnDeleteTrackedFoodClick(val trackedFood: TrackedFood): TrackerOverviewEvent()
+    data class OnAddFoodClick(val meal: Meal): TrackerOverviewEvent()
+}
