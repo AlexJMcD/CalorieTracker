b60fc7c3048da78ba7f2dd54840335ae0a758bc2
AlexJMcD <AlexMcD@protonmail.com>
2022-08-30 14:45:57 +0100

    The day selector for the tracker screen. Allows you to cycle through dates and when the tracked food section is added, it will allow you to see the tracked food for specific dates.

--- tracker/tracker_presentation/src/main/java/com/am/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/am/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,12 +1,12 @@
 package com.am.tracker_presentation.tracker_overview
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.am.tracker_presentation.tracker_overview.tracker_components.DaySelector
 import com.am.tracker_presentation.tracker_overview.tracker_components.NutrientsHeader
 import com.amcoding.core_ui.LocalSpacing
 import com.plcoding.core.util.UiEvent
@@ -23,9 +23,21 @@ fun TrackerOverviewScreen(
         modifier = Modifier
             .fillMaxSize()
             .padding(bottom = spacing.spaceMedium)
+
     ){
         item{
             NutrientsHeader(state = state)
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                },
+                onNextDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                },
+                modifier = Modifier.fillMaxWidth().padding(spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
 }
