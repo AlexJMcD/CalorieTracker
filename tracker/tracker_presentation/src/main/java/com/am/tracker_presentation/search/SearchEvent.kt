85149b5fbad49db7a1343511a16e08130902bb83
AlexJMcD <AlexMcD@protonmail.com>
2022-09-02 09:59:41 +0100

    Created the search viewmodel, which can handle the various user interactions that will happen in the searchUi, such as searching for foods, changing focus on the page, clicking on a food etc.

---
+++ tracker/tracker_presentation/src/main/java/com/am/tracker_presentation/search/SearchEvent.kt
@@ -0,0 +1,14 @@
+package com.am.tracker_presentation.search
+
+import com.am.tracker_domain.model.MealType
+import com.am.tracker_domain.model.TrackableFood
+import java.time.LocalDate
+
+sealed class SearchEvent{
+    data class OnQueryChange(val query: String): SearchEvent()
+    object OnSearch: SearchEvent()
+    data class OnToggleTrackableFoods(val food: TrackableFood): SearchEvent()
+    data class OnAmountForFoodChange(val food: TrackableFood, val amount: String): SearchEvent()
+    data class OnTrackedFoodClick(val food: TrackableFood, val mealType: MealType, val date: LocalDate): SearchEvent()
+    data class OnSearchFocusChange(val isFocused: Boolean): SearchEvent()
+}
---
+++ tracker/tracker_presentation/src/main/java/com/am/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.am.tracker_presentation.search
+
+import com.am.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
